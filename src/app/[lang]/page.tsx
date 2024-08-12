9b14d326a85ff1a55ddb4af875a21195fed0b3b8
أحمد علاء الدين عادل أحمد <136151580+SinSintro@users.noreply.github.com>
2024-08-12 11:24:04 +0300

    Update page.tsx

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -11,5 +11,6 @@ export const metadata: Metadata = { title: "Home" };
 export default async function Home({ params: { lang } }: HomeProps) {
   const { user } = await getAuth();
   if (user) redirect(`/${lang}/dashboard`);
+  else redirect(`/${lang}/login`);
   return <div className="container flex-1 py-6">Home </div>;
 }

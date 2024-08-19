a81ff2da4bd40ad3768234f6524bb587747d49b7
أحمد علاء الدين عادل أحمد <ahmedzzz456@gmail.com>
2024-08-19 09:01:31 +0300

    update

--- src/lib/dictionaries.ts
+++ src/lib/dictionaries.ts
@@ -4,12 +4,12 @@ import { Locale } from "@/types/locale";
 import { translateObject } from "@/lib/locale";
 
 const site = {
-  // ar: () => import("@/dictionaries/ar.json").then((module) => module.default),
+  ar: () => import("@/dictionaries/ar.json").then((module) => module.default),
   en: () => import("@/dictionaries/en.json").then((module) => module.default),
 };
 
 export const getDictionary = cache(async (locale: Locale) => {
-  // if (locale === "ar" || locale === "en") return await site[locale]();
+  if (locale === "ar" || locale === "en") return await site[locale]();
 
   const dic = await site["en"]();
   return (await translateObject(dic as any, locale)) as unknown as typeof dic;

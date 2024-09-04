57611a563dbaed5f3c290dbfc49efd3c26da9dae
أحمد علاء الدين عادل أحمد <ahmedzzz456@gmail.com>
2024-09-05 02:27:10 +0300

    posts update

--- src/actions/posts.ts
+++ src/actions/posts.ts
@@ -255,7 +255,6 @@ export async function createPost(
             platform: acc,
             postAt: new Date(currentDate),
             imageId: imageData?.["id"] ?? null,
-            image: imageData,
           });
           indicator++;
         });

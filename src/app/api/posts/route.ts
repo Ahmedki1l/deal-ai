860624b7b5826738ca5fbbdd8f51251d7440a530
أحمد علاء الدين عادل أحمد <ahmedzzz456@gmail.com>
2024-09-05 02:12:47 +0300

    api/posts update

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -4,26 +4,21 @@ import { createPost } from "@/actions/posts";
 
 export async function GET(req: NextRequest) {
   const key = req.nextUrl.searchParams.get("key");
-  if (!key) return new NextResponse("Invalid request", { status: 400 });
+  if (!key) return new NextResponse("Post not found", { status: 404 });
 
-  try {
-    const stream = new ReadableStream({
-      async start(controller) {
-        await createPost(controller, key);
-      },
-    });
+  const stream = new ReadableStream({
+    async start(controller) {
+      createPost(controller, key).finally(() => {
+        cookies().delete(key);
+      });
+    },
+  });
 
-    return new NextResponse(stream, {
-      headers: {
-        "Content-Type": "text/event-stream",
-        "Cache-Control": "no-cache",
-        Connection: "keep-alive",
-      },
-    });
-  } catch (error) {
-    console.error("Failed to start stream:", error);
-    return new NextResponse("Server error", { status: 500 });
-  } finally {
-    cookies().delete(key);
-  }
+  return new NextResponse(stream, {
+    headers: {
+      "Content-Type": "text/event-stream",
+      "Cache-Control": "no-cache",
+      Connection: "keep-alive",
+    },
+  });
 }

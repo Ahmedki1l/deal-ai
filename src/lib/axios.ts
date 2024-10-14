74016e78a5a5f9227339dfd3d45b6eb671ae33d0
Lamiaa Gebriel <lamiaadev@gmail.com>
2024-10-14 19:29:35 +0300

    axios timing log

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,9 +1,9 @@
 import { Locale } from "@/types/locale";
-import axiosInstance from "axios";
+import Axios from "axios";
 import { User } from "lucia";
 
-const axios = ({ locale, user }: { locale: Locale; user: User | null }) =>
-  axiosInstance.create({
+const axios = ({ locale, user }: { locale: Locale; user: User | null }) => {
+  const axiosInstance = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_SERVER_BASE_URL,
     // timeout: 5000,
     headers: {
@@ -12,5 +12,35 @@ const axios = ({ locale, user }: { locale: Locale; user: User | null }) =>
       "Content-Type": "application/json",
     },
   });
+
+  axiosInstance.interceptors.request.use(
+    (config: any) => {
+      // Store the start time before making the request
+      config.metadata = { startTime: new Date() };
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    },
+  );
+
+  axiosInstance.interceptors.response.use(
+    (response: any) => {
+      // Calculate the time taken for the request
+      const endTime: any = new Date();
+      const timeTaken = endTime - response.config.metadata.startTime;
+      console.log(`Request to ${response.config.url} took ${timeTaken}ms`);
+      return response;
+    },
+    (error) => {
+      const endTime: any = new Date();
+      const timeTaken = endTime - error.config.metadata.startTime;
+      console.log(`Request to ${error.config.url} failed, took ${timeTaken}ms`);
+      return Promise.reject(error);
+    },
+  );
+
+  return axiosInstance;
+};
 
 export default axios;
